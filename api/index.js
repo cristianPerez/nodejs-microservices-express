bff0f6b7e52ea41e446892e1fc4def012c6fad44
Cperez354 <cperez354@gmail.com>
2020-07-15 11:17:31 -0500

    "feat: New swagger docummentation."

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const swaggerUi  =require('swagger-ui-express');
 
 const config = require('../config.js');
 const user = require('./components/user/network');
+const swaggerDoc = require('./swagger.json');
 
 const app = express();
 const  { log } = console;
@@ -10,7 +12,8 @@ const  { log } = console;
 app.use(bodyParser.json())
 
 // Router
-app.use('/api/user', user)
+app.use('/api/user', user);
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.listen(config.api.port, () => {
   log('Listen on port', config.api.port);
